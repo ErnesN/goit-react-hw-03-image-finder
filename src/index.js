f898c73a0453d4e0f53e401b571796b115b8d274
Ernes Nafieiev <demodekoz@gmail.com>
2023-02-02 23:34:47 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ImagesSearch from './components/ImagesSearch/ImagesSearch';
+import ImagesSearch from './modules/ImagesSearch/ImagesSearch';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/shared/services/images-api.js
+++ src/shared/services/images-api.js
@@ -3,18 +3,20 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://pixabay.com/api',
   params: {
-    _key: '32003673-6678ea5058f0970b487cd30b2',
-    _limit: 12,
-    _image_type: 'photo',
-    _orientation: 'horizontal',
+    q: '',
+    page: 1,
+    key: '32003673-6678ea5058f0970b487cd30b2',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: 12,
   },
 });
 
-export const searchImages = async (q, _page = 1) => {
+export const searchImages = async (q, page = 1) => {
   const { data } = await instance.get('/', {
     params: {
       q,
-      _page,
+      page,
     },
   });
   return data;
